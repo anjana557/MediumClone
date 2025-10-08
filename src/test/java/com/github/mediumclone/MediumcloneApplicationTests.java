5d23b2d257ca69b348d965ba7bc73178f33671bc
anjana557 <anjana.sitamarhi007@gmail.com>
2025-10-08 18:44:31 +0530

    first commit

---
+++ src/test/java/com/github/mediumclone/MediumcloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.mediumclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediumcloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
