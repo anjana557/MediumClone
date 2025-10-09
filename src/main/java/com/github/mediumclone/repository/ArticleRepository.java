1a6858434a410f4422639ec089728933b3b66859
anjana557 <anjana.sitamarhi007@gmail.com>
2025-10-09 17:02:01 +0530

    removed @repository

--- src/main/java/com/github/mediumclone/repository/ArticleRepository.java
+++ src/main/java/com/github/mediumclone/repository/ArticleRepository.java
@@ -2,8 +2,6 @@ package com.github.mediumclone.repository;
 
 import com.github.mediumclone.entity.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface ArticleRepository extends JpaRepository<Article,Integer> {
 }
